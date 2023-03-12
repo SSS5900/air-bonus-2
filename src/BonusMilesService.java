4042145422abe7911c4d2f7d1b552e520a00c603
SSS5900 <sosnovskihsergey@gmail.com>
2023-03-12 19:28:01 +0500

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,15 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+
+        int priceMile = 20;
+
+        int mile;
+        mile = price / priceMile;
+
+        return mile;
+
+    }
+
+
+}
